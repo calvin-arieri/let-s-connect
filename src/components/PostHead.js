d0133b30474db5c8212e5599c09788b904da2a4d
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-05-02 16:48:58 +0300

    making the basic structure of header

--- src/components/PostHead.js
+++ src/components/PostHead.js
@@ -1,1 +1,21 @@
 import React from "react";
+
+function PostHeader() {
+    return(
+        <div>
+            <div id="image">
+                <img src="" alt=""/>
+            </div>
+            <div id="other_info">
+                <span>Name</span>
+                <span>About</span>
+                <br/>
+                <br/>
+                <br/>
+                <nav>
+                    Posts
+                </nav>
+            </div>
+        </div>
+    )
+}
