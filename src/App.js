25e8d7f2f01178d1e6b122fc26b3521fadab7f18
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-05-02 23:28:56 +0300

    rendered the posts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import SignUp from './components/SignUp';
+//import HomePage from './components/HomePage';
+//import SignUp from './components/SignUp';
+import Post from './components/Posts';
 
 function App() {
 
-  function handleSignUp() {
-  // Add your sign-up logic here
-  console.log("User signed up successfully!");
-}
+//   function handleSignUp() {
+//   // Add your sign-up logic here
+//   console.log("User signed up successfully!");
+// }
   
   return (
     <div className="App">
-   
-
-   <SignUp handleSignUp={handleSignUp}/>
+      {/* <HomePage /> */}
+      <Post />   
+   {/* <SignUp handleSignUp={handleSignUp}/> */}
     </div>
   );
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 function Post() {
+    const [available_posts , getPost] = useState([])
+    useEffect(() =>{
+        fetch(`https://lets-connect-bryn.onrender.com/posts`)
+        .then(r => r.json())
+        .then(posts => getPost(posts))
+    }, [])
+
+    //console.log(available_posts)
     return(
         <div>
-            
+            {available_posts.map((one_post) =>{
+                const {media, userName, caption, dislike, like} = one_post
+                return(
+                    <div className="card">
+                        <p>{userName}</p>
+                        <img src={media} alt={userName}/>
+                        <p>{caption}</p>
+                        <p><span>{like}</span><span>{dislike}</span></p>
+                    </div>
+                )
+            })}
         </div>        
     )
 }
+
+export default Post;
