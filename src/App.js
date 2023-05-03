0f82f70afd18c6b5b12bf02f3a2ae298e1c7e3d5
BETHWEL <bethwelk85@gmail.com>
2023-05-03 14:46:54 +0300

    placing displayProfiles inside components folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import ProfilesDisplay from "./displayProfiles";
+
 
 function App() {
- 
+  //   function handleSignUp() {
+  //   // Add your sign-up logic here
+  //   console.log("User signed up successfully!");
+  // }
+
 
   return <div className="App">
-    <ProfilesDisplay />
+    
     
   </div>;
 }
