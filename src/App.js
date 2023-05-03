f7b4b47c88b7d0df99fea9454d221d6a8e55dba0
calvin-arieri <calvin.arieri@student.moringaschoool.com>
2023-05-03 23:44:45 +0300

    calvin changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import "./App.css";
-<<<<<<< HEAD
-=======
 import Suggestion from "./components/Suggestion";
 //import ProfilesDisplay from "./components/displayProfiles";
 
 
 
->>>>>>> 4c6bf3eb5b0a0a5425dcba3f195dde41c6fc7638
 
 function App() {
   //   function handleSignUp() {
@@ -14,21 +11,11 @@ function App() {
   //   console.log("User signed up successfully!");
   // }
 
-<<<<<<< HEAD
-  return (
-    <div className="App">
-      {
-
-      
-    </div>
-  );
-=======
 
   return <div className="App">
     {/* <ProfilesDisplay /> */}
     <Suggestion maximum_age={30} minimum_age={18} location="NAIROBI" preferred_gender={"female"} interest={"PARTYING"} type_relationship={"long-term"}/>    
   </div>;
->>>>>>> 4c6bf3eb5b0a0a5425dcba3f195dde41c6fc7638
 }
 
 export default App;
