ced5c27df4b11a3b7d3599bf6845eccce8efc58f
BETHWEL <bethwelk85@gmail.com>
2023-05-03 22:45:06 +0300

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import "./App.css";
+import './App.css';
+
 
 
 function App() {
-  //   function handleSignUp() {
-  //   // Add your sign-up logic here
-  //   console.log("User signed up successfully!");
-  // }
+  return (
+    <div className="App">
+      
+      
+    
 
 
-  return <div className="App">
-    
-    
-  </div>;
+    </div>
+  );
 }
 
 export default App;
